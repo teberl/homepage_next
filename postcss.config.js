0df245aac5114dd99fdf2772c9db53e5f3412b8a
teberl <mail@teberl.de>
2020-02-22 23:48:46 +0100

    Try to purge tailwind better

--- postcss.config.js
+++ postcss.config.js
@@ -1,17 +1,15 @@
-const plugins = ["tailwindcss", "postcss-preset-env"];
-
-if (process.env.NODE_ENV === "production")
-  plugins.concat([
-    "@fullhuman/postcss-purgecss",
-    {
-      content: [
-        "./pages/**/*.{js,jsx,ts,tsx}",
-        "./components/**/*.{js,jsx,ts,tsx}",
-      ],
-      defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
-    },
-  ]);
-
 module.exports = {
-  plugins,
+  plugins: [
+    "tailwindcss",
+    "autoprefixer",
+    [
+      "@fullhuman/postcss-purgecss",
+      process.env.NODE_ENV === "production"
+        ? {
+            content: ["./pages/**/*.tsx", "./components/**/*.tsx"],
+            defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+          }
+        : false,
+    ],
+  ],
 };
