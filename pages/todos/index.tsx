b8afc5ec55861b0391c284437e6e4d9cdf3ab516
teberl <mail@teberl.de>
2020-02-25 21:11:44 +0100

    Remove context

--- pages/todos/index.tsx
+++ pages/todos/index.tsx
@@ -1,13 +1,10 @@
 import { NextPage } from "next";
 import Layout from "../../components/Layout";
 import MainSection from "../../components/todos/MainSection";
-import { TodosCtxProvider } from "../../components/todos/context";
 
 const About: NextPage = () => (
   <Layout>
-    <TodosCtxProvider>
-      <MainSection />
-    </TodosCtxProvider>
+    <MainSection />
   </Layout>
 );
 
