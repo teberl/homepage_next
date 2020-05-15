68f3acf514010c3eb5fa76640747e775e94d7512
teberl <mail@teberl.de>
2020-05-15 22:00:03 +0200

    Fix component name

--- pages/todos/index.tsx
+++ pages/todos/index.tsx
@@ -3,10 +3,10 @@ import Layout from "../../components/Layout";
 import TodosMain from "../../components/todos/TodosMain";
 import {} from "../../components/todos/context";
 
-const About: NextPage = () => (
+const Todos: NextPage = () => (
   <Layout>
     <TodosMain />
   </Layout>
 );
 
-export default About;
+export default Todos;
